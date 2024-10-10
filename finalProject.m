de894923ebac1fdda423933d68c7a372d88e18ec
ayano-ueki <aueki@ou.edu>
2024-10-10 10:20:35 -0500

    Update finalProject.m

--- finalProject.m
+++ finalProject.m
@@ -1,3 +1,9 @@
+% ECE 5523: Random Signals - Final Project
+% Team Members: Breanna Mapes, Lucia Torres, Ayano Ueki
+%
+% Description:
+% On Digital Filtering for Noise Reduction in Audio Samples
+
 % load in audio data
 cleanWoman = audioread('lucia_clean.wav');
 cleanMan = audioread('cody_clean.wav');
