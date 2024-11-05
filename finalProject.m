8c61a88808a623ce6946cd21e72520b46095f1c8
ayano-ueki <aueki@ou.edu>
2024-11-05 09:28:22 -0600

    Update finalProject.m
    Add traffic noise

--- finalProject.m
+++ finalProject.m
@@ -7,6 +7,7 @@
 % Step 1: Load the original WAV files without noise
 [cleanWoman, fs_cw] = audioread('lucia_clean.wav');
 [cleanMan, fs_cm] = audioread('cody_clean.wav');
+[trafficNoise, fs_tn] = audioread('sound_traffic.wav');
 
 % Step 2: Play the original WAV file without noise
 playSignal(cleanWoman, fs_cw, 'cleanWoman')
@@ -21,7 +22,7 @@ playSignal(gaussianSignal, fs_cw, 'gaussianWoman');
 audiowrite('gaussianWoman.wav', gaussianSignal, fs_cw);
 disp('Noise-added WAV file saved as "gaussianWoman.wav".');
 
-% repeat steps 3-5 for other noises
+% repeat steps 3-5 for Rayleigh noises
 %% Rayleigh
 % step 3: Add Rayleigh noise to signals
 rayleighWoman = addRayleighNoise(cleanWoman);
@@ -54,3 +55,13 @@ title("Clean audio sample (man)");
 subplot(2,1,2);
 plot(rayleighMan);
 title("Noisy audio sample (man)");
+
+% repeat steps 3-5 for traffic noises
+trafficWoman = addTrafficNoise(cleanWoman);
+
+% step 4: Play the noise-added WAV file
+playSignal(trafficWoman, fs_cw, 'trafficWoman');
+
+% step 5: Save the noise-added audios to new files
+audiowrite('trafficWoman.wav',trafficWoman,fs_cw);
+disp('Noise-added WAV file saved as "trafficWoman.wav".');
