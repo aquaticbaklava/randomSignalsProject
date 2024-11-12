2da7f2c674bd547e95fde9472df2a5997e3e9ced
Breanna Mapes <72283868+aquaticbaklava@users.noreply.github.com>
2024-11-12 08:48:23 -0600

    Updated for SNR

--- addExponentialNoise.m
+++ addExponentialNoise.m
@@ -1,6 +1,6 @@
-function [noisySignal, ratio] = addExponentialNoise(signal)
+function [noisySignal, ratio] = addExponentialNoise(signal, SNR)
     x = rand(length(signal),1);
-    pnoise = snr_noise(15,signal,'E');
+    pnoise = snr_noise(SNR,signal,'E');
     exponential = -1*log(1-x).*pnoise(1);
     noisySignal = signal + exponential;
     ratio = snr(signal(:,1),exponential);
