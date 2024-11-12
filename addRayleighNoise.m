680c430773ade6ad794cf08294db6214e44c9c1d
Breanna Mapes <72283868+aquaticbaklava@users.noreply.github.com>
2024-11-12 08:46:20 -0600

    Can now set SNR

--- addRayleighNoise.m
+++ addRayleighNoise.m
@@ -1,5 +1,8 @@
-function noisySignal = addRayleighNoise(signal)
+function [noisySignal,ratio] = addRayleighNoise(signal, SNR)
     x = rand(length(signal),1); % make uniform first
-    rayleigh = sqrt(-2.*log(x)); % get rayleigh from uniform
+    pnoise = snr_noise(SNR,signal,'R');
+    rayleigh = sqrt(-2.*log(x)).*pnoise(1); % get rayleigh from uniform
+    % get SNR and multiply to get what we want
+    ratio = snr(signal(:,1),rayleigh);
     noisySignal = signal + rayleigh;
 end
