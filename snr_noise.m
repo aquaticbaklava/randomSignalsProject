30de25ca9cc88dbf45b7105e1f1d6a70908ce986
Breanna Mapes <72283868+aquaticbaklava@users.noreply.github.com>
2024-11-12 08:43:42 -0600

    Update snr_noise.m for Rayleigh and Exponential

--- snr_noise.m
+++ snr_noise.m
@@ -5,6 +5,9 @@ function P_noise = snr_noise(SNR, signal, type)
 % type: type of noise
 % Gaussian: actual SNR close to ideal SNR
 % Uniform: actual SNR ends up being around ~4.5dB higher
+% Exponential: actual SNR ends up being around ~3dB lower
+% Rayleigh: actual SNR ends up being around ~3dB lower
+
 
 P_noise = std(signal) / db2mag(SNR);
 % Gaussian Random Variable
@@ -20,11 +23,13 @@ end
 
 % Exponential Random Variable
 if type == 'E'
-    
+    P_noise = pow2db(P_noise) - 3/2;
+    P_noise = db2pow(P_noise);
 end
 % Rayleigh Random Varaible
 if type == 'R'
-
+    P_noise = pow2db(P_noise) - 3/2;
+    P_noise = db2pow(P_noise);
 end
 
 end
