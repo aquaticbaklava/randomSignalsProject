92c9b68cedbec21e58b96ced364156dab1abb731
luwaddles987 <89464035+luwaddles987@users.noreply.github.com>
2024-11-14 10:30:53 -0600

    FIXED UP

--- lpf_averaging.m
+++ lpf_averaging.m
@@ -3,13 +3,20 @@ function rec_sig = lpf_averaging(signal)
 % signal: noisy signal
 
 samps = length(signal);
-samps_pad = padarray(noisy_signal,3); % zero pad for special cases
+samps_pad = padarray(signal,3); % zero pad for special cases
 
 rec_sig = zeros(size(samps_pad)); % size of zero padded image
 for s = (1:samps)+3 % actual image
     % linear average filter at every sample
     rec_sig(s) = sum(sum(samps_pad(s-3:s+3)))/7; 
 end
+rec_sig = rec_sig(1+3:end-3);
+
+figure;
+plot(rec_sig);
+title('Linear Low-Pass Average Filter');
+recovered_audio_sample = audioplayer(rec_sig, fs);
+playblocking(recovered_audio_sample);
 
 end
 
