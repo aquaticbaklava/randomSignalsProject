94e83b5ef4c35537fe1cd86121926571f6035afd
Breanna Mapes <72283868+aquaticbaklava@users.noreply.github.com>
2024-11-19 11:21:54 -0600

    Update bsf.m

--- bsf.m
+++ bsf.m
@@ -1,6 +1,6 @@
 function filteredSig = bsf(noisySig,snr)
     % normalize sig
     noisySig = noisySig/max(noisySig(:,1));
-    bounds = [max(noisySig(:,1))/snr max(noisySig(:,1))];
+    bounds = [max(noisySig(:,1))/snr max(noisySig(:,1))-.00001];
     filteredSig = bandstop(noisySig,bounds);
 end
