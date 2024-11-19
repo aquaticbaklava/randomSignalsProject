a7b1b3bc31add6a920aee7891534e4499a674db4
Breanna Mapes <72283868+aquaticbaklava@users.noreply.github.com>
2024-11-19 10:18:48 -0600

    Update buttfilt.m

--- buttfilt.m
+++ buttfilt.m
@@ -1,8 +1,9 @@
 function filteredSig = buttfilt(noisySig,snr)
+    % normalize sig
+    noisySig = noisySig/max(noisySig(:,1));
     wp = max(noisySig(:,1))/pi;
-    % use snr to get adaptive ws
     ws = 0.45/pi/snr;
-    rp =1;
+    rp = 1;
     rs = 30;
     [n,Wn]=buttord(wp,ws,rp,rs);
     [num,den] = butter(n,Wn,"low");
