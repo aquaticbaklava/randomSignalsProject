32b91a17e7a13f37a74d82accb69305bd2212be3
Breanna Mapes <72283868+aquaticbaklava@users.noreply.github.com>
2024-12-02 11:33:03 -0600

    Create plotSpectrum.m for freq spectrums

---
+++ plotSpectrum.m
@@ -0,0 +1,10 @@
+function plotSpectrum(cleanSignal, fs, titleWords)
+    N = length(cleanSignal);
+    [freqSignal, freq] = easyFFT(cleanSignal,N,1,fs);
+    freqSignal = abs(freqSignal);
+    figure;
+    plot(freq,freqSignal(:,1));
+    xlabel("Frequency (Hz)");
+    ylabel("Power Spectrum Density");
+    title(titleWords)
+end
