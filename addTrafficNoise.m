a68a6ea522194e04ffd55fc7784ee65eea6c3324
Breanna Mapes <72283868+aquaticbaklava@users.noreply.github.com>
2024-12-03 13:26:59 -0600

    Update addTrafficNoise.m

--- addTrafficNoise.m
+++ addTrafficNoise.m
@@ -2,29 +2,35 @@
 % Function to add Traffic Noise
 %
 
-function [noisySignal,ratio] = addTrafficNoise(signal, fs_s)
-    [trafficNoise, fs_tn] = audioread('sound_traffic.wav');
+function [noisySignal, ratio] = addTrafficNoise(signal, fs_s, as)
+   [trafficNoise, fs_tn] = audioread('sound_traffic.wav');
+   % If the sampling frequencies are different,
+   % resample trafficNoise to match cleanWoman
+   if fs_s ~= fs_tn
+       trafficNoise = resample(trafficNoise, fs_s, fs_tn);
+       fs_tn = fs_s;
+   end
+   % Get the lengths of the signals
+   len_s = length(signal);
+   len_tn = length(trafficNoise);
+   % Select a segment from trafficNoise with the same length as signal
+   start_index = randi([1, len_tn - len_s + 1]);
+   % to make sure SNR is between 5 and 15 dB
+   if as == 1
+       trafficNoise_added = 1.2*trafficNoise(start_index:start_index + len_s - 1, 1);
+   else
+       trafficNoise_added = 0.9*trafficNoise(start_index:start_index + len_s - 1, 1);
+   end
+   % FIGURE: traffic noise
+   figure;
+   plot(trafficNoise_added);
+   title('Traffic Noise');
+   exportgraphics(gca,['T_hist_',num2str(as),'.png']);
+   ratio = snr(signal, trafficNoise_added);
+   % Add the noise segment to cleanWoman
+   noisySignal = signal + trafficNoise_added;
+   % Normalize the noisy signal if needed (optional)
+   noisySignal = noisySignal / max(abs(noisySignal(:)));
 
-    % If the sampling frequencies are different,
-    % resample trafficNoise to match cleanWoman
-    if fs_s ~= fs_tn
-        trafficNoise = resample(trafficNoise, fs_s, fs_tn);
-        fs_tn = fs_s;
-    end
-
-    % Get the lengths of the signals
-    len_s = length(signal);
-    len_tn = length(trafficNoise);
-
-    plotSpectrum(trafficNoise,fs_s,"Frequency Spectrum for Traffic Noise")
-
-    % Select a segment from trafficNoise with the same length as signal
-    start_index = randi([1, len_tn - len_s + 1]);
-    trafficNoise_added = trafficNoise(start_index:start_index + len_s - 1, :);
-    ratio = snr(signal(:,1),trafficNoise_added(:,1));
-    % Add the noise segment to cleanWoman
-    noisySignal = signal + trafficNoise_added;
-
-    % Normalize the noisy signal if needed (optional)
-    noisySignal = noisySignal / max(abs(noisySignal(:)));
+   plotSpectrum(trafficNoise,fs_s,"Frequency Spectrum for Traffic Noise");
 end
