a494a88d02c8a808dbb1a21995ec01f0e458ee21
Breanna Mapes <72283868+aquaticbaklava@users.noreply.github.com>
2024-12-03 13:25:37 -0600

    Update addGaussianNoise.m

--- addGaussianNoise.m
+++ addGaussianNoise.m
@@ -1,21 +1,18 @@
 % ECE 5523: Random Signals - Final Project
 % Function to add Gaussian Noise
-%
-function [noisySignal,ratio] = addGaussianNoise(signal, snr_ideal, fs)
-    P_noise = snr_noise(snr_ideal,signal, 'G');
+function [noisySignal,  ratio] = addGaussianNoise(signal, snr_ideal, as, fs)
+   P_noise = snr_noise(snr_ideal, signal, 'G');
+   meanNoise = 0;        % Mean of the noise
+   varianceNoise = 1; % Variance of the noise
+   noise = P_noise*(meanNoise + sqrt(varianceNoise) * randn(size(signal)));
+   % FIGURE: gaussian noise
+   figure;
+   histfit(noise,20,'normal');
+   title('Gaussian Noise');
+   exportgraphics(gca,['G_hist_',num2str(snr_ideal),'_',num2str(as),'.png']);
+   ratio = snr(signal,noise);
+   noisySignal = signal + noise;
 
-    meanNoise = 0;        % Mean of the noise
-    varianceNoise = 0.01; % Variance of the noise
-
-    noise = P_noise.*(meanNoise + sqrt(varianceNoise) .* randn(size(signal)));
-    % FIGURE: gaussian noise
-    figure;
-    histogram(noise, 'normalization','pdf');
-    title('Gaussian Noise');
-
-    plotSpectrum(noise,fs,"Frequency Spectrum for Gaussian Noise")
-    
-    ratio = snr(signal(:,1),noise(:,1));
-    noisySignal = signal + noise;
+   plotSpectrum(noise,fs,"Frequency Spectrum for Gaussian Noise");
 end
 
