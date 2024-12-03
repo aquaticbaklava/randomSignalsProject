088190e69b7b85853b2e0bb31c9655ce1ea943a6
Breanna Mapes <72283868+aquaticbaklava@users.noreply.github.com>
2024-12-03 13:24:23 -0600

    Update addConversationNoise.m

--- addConversationNoise.m
+++ addConversationNoise.m
@@ -1,30 +1,35 @@
 % ECE 5523: Random Signals - Final Project
-% Function to add Traffic Noise
-%
+% Function to add Conversation Noise
+function [noisySignal, ratio] = addConversationNoise(signal, fs_s, as)
+   [conversationNoise, fs_tn] = audioread('sound_conversation2.wav');
+   % If the sampling frequencies are different,
+   % resample trafficNoise to match cleanWoman
+   if fs_s ~= fs_tn
+       conversationNoise = resample(conversationNoise, fs_s, fs_tn);
+       fs_tn = fs_s;
+   end
+   % Get the lengths of the signals
+   len_s = length(signal);
+   len_tn = length(conversationNoise);
+   % Select a segment from trafficNoise with the same length as signal
+   start_index = randi([1, len_tn - len_s + 1]);
+   if as == 1
+       conversationNoise_added = .68*conversationNoise(start_index:start_index + len_s - 1, 1);
+   else
+       conversationNoise_added = .47*conversationNoise(start_index:start_index + len_s - 1, 1);
+   end
+   % FIGURE: conversation noise
+   figure;
+   plot(conversationNoise_added);
+   title('Background Conversation Noise');
+   exportgraphics(gca,['C_hist_',num2str(as),'.png']);
 
-function noisySignal = addConversationNoise(signal, fs_s)
-    [conversationNoise, fs_tn] = audioread('sound_conversation2.wav');
+   plotSpectrum(conversationNoise,fs_s,"Frequency Spectrum for Conversation Noise");
 
-    % If the sampling frequencies are different,
-    % resample trafficNoise to match cleanWoman
-    if fs_s ~= fs_tn
-        conversationNoise = resample(conversationNoise, fs_s, fs_tn);
-        fs_tn = fs_s;
-    end
-
-    % Get the lengths of the signals
-    len_s = length(signal);
-    len_tn = length(conversationNoise);
-
-    % Select a segment from trafficNoise with the same length as signal
-    start_index = randi([1, len_tn - len_s + 1]);
-    conversationNoise_added = conversationNoise(start_index:start_index + len_s - 1, :);
-
-    plotSpectrum(conversationNoise,fs_s,"Frequency Spectrum for Conversation Noise")
-
-    % Add the noise segment to cleanWoman
-    noisySignal = signal + conversationNoise_added;
-
-    % Normalize the noisy signal if needed (optional)
-    noisySignal = noisySignal / max(abs(noisySignal(:)));
+  
+   ratio = snr(signal,conversationNoise_added);
+   % Add the noise segment to cleanWoman
+   noisySignal = signal + conversationNoise_added;
+   % Normalize the noisy signal if needed (optional)
+   noisySignal = noisySignal / max(abs(noisySignal(:)));
 end
